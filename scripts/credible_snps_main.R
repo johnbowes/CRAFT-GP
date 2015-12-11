56cc4e85e7f8f010490c5cea5b19184128034736
johnbowes <j.bowes@manchester.ac.uk>
2015-12-11 15:30:52 +0000

    updated credible_snps_main.R

--- scripts/credible_snps_main.R
+++ scripts/credible_snps_main.R
@@ -22,30 +22,46 @@ opt <- parse_args(OptionParser(option_list=option_list))
 
 # read regions file
 regions <- read_delim(opt$regions, delim = "\t", col_names = c('index','locus')) %>%
-	separate(locus, sep = "[:-]", c('chr','start','end')
+	separate(locus, sep = "[:-]", c('chr','start','end'))
 
 # read summary statistics
 data <- read_delim(opt$stats, delim = "\t")
 
-	
 # calculate ABF
 data$BF <- -abf(p=data$PVAL, maf=data$MAF, n0=opt$affected, n1=opt$unaffected)
 
 # define credible SNP sets
-cred99 <- cred95 <- summ  <- vector("list",1)
 
-psub <- pproc(data, opt$snp) %>%
-	mutate(index_snp=opt$snp)
+cred99 <- cred95 <- summ  <- vector("list",nrow(regions))
 
-sm <- dosumm(psub)
+for(i in seq_along(cred99)) {
 
-cred95 <- psub[1:sm["n95"], c("SNPID","index_snp","CHR","POS","MAF","PVAL","index","pp","cpp")]
-cred99 <- psub[1:sm["n99"], c("SNPID","index_snp","CHR","POS","MAF","PVAL","index","pp","cpp")]
-summ <- c(narrow=sm)
+	current_index <- as.character(regions[i,1])
+	psub <- pproc(subset(data,indexSNP==current_index), current_index)
 
-# write results to file
-cred95_file <- paste(opt$snp, "credible_snps_95.txt", sep='_')
-cred99_file <- paste(opt$snp, "credible_snps_99.txt", sep='_')
+	sm <- dosumm(psub)
+	cred95[[i]] <- psub[1:sm["n95"], c("SNPID","CHROM","POS","MAF","PVAL","indexSNP","pp","cpp")]
+	cred99[[i]] <- psub[1:sm["n99"], c("SNPID","CHROM","POS","MAF","PVAL","indexSNP","pp","cpp")]
+	summ[[i]] <- c(narrow=sm)
 
-write_tsv(cred95, cred95_file)
-write_tsv(cred99, cred99_file)
+}
+
+# create summary table
+result <- data.frame(indexSNP = un_inSNP$indexSNP, do.call("rbind",summ), stringsAsFactors =FALSE)
+result$cred95.start <- sapply(cred95,function(x) min(x$POS))
+result$cred95.end <- sapply(cred95,function(x) max(x$POS))
+result$cred99.start <- sapply(cred99,function(x) min(x$POS))
+result$cred99.end <- sapply(cred99,function(x) max(x$POS))
+result$cred95.interval <- result$cred95.end - result$cred95.start
+result$cred99.interval <- result$cred99.end - result$cred99.start
+
+write_delim(result, "output/credible_snps/summary_table.txt")
+
+# create credible snp table
+cred99.long <- do.call("rbind",cred99)
+
+write_delim(cred99.long, "output/credible_snps/credible_snps.txt")
+
+# create bed files
+
+#write_delim(cred99.long, "output/credible_snps/credible_snps.bed")
